97e689e24834a5e7e3d7264fc0bb0586a80e6444
k0t3n <k0t3n.mail@gmail.com>
2019-10-25 11:44:27 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='changelly_api',
+    version='0.0.1',
+    packages=['changelly_api'],
+    url='https://github.com/k0t3n/changelly_api',
+    license='MIT',
+    author='k0t3n',
+    author_email='k0t3n.mail@gmail.com',
+    description='Python wrapper library over Changelly RPC API'
+)
